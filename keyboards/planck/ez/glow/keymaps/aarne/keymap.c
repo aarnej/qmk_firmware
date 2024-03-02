6ae0d1d068b34a23f58e972f7ed7da7aa1e4ad7a
aarnej <aarnej@iki.fi>
2024-03-02 18:01:27 +0200

    us layout + unicode

--- keyboards/planck/ez/glow/keymaps/aarne/keymap.c
+++ keyboards/planck/ez/glow/keymaps/aarne/keymap.c
@@ -18,58 +18,69 @@ enum planck_layers {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
-#define HYPER(KEY) LALT(LGUI(LCTL(LSFT(KEY))))
 #define SLEEP KC_SYSTEM_SLEEP
 #define PREV_TRACK KC_MEDIA_PREV_TRACK
 #define NEXT_TRACK KC_MEDIA_NEXT_TRACK
 #define PAUSE KC_MEDIA_PLAY_PAUSE
 
 enum {
-    TD_ESC_Q,
+    ADIA_LOWER,
+    ADIA_UPPER,
+    ODIA_LOWER,
+    ODIA_UPPER,
+    ARING_LOWER,
+    ARING_UPPER,
 };
 
-// Tap Dance definitions
-tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_ESC_Q] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
+const uint32_t PROGMEM unicode_map[] = {
+    [ADIA_LOWER] = 0xe4,
+    [ADIA_UPPER] = 0xc3,
+    [ODIA_LOWER] = 0xf6,
+    [ODIA_UPPER] = 0xd6,
+    [ARING_LOWER] = 0xe5,
+    [ARING_UPPER] = 0xc5
 };
+
+#define ADIA UP(ADIA_LOWER, ADIA_UPPER)
+#define ODIA UP(ODIA_LOWER, ODIA_UPPER)
+#define ARING UP(ARING_LOWER, ARING_UPPER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_planck_grid(
-        XXXXXXX,        TD(TD_ESC_Q),   KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           SE_ADIA,
-        KC_TAB,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,           KC_H,           LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   SE_SCLN,        SE_OSLH,
-        XXXXXXX,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         SE_SLSH,        XXXXXXX,
-        CW_TOGG,        XXXXXXX,        XXXXXXX,        XXXXXXX,        RAISE,          KC_SPACE,       XXXXXXX,        LOWER,          XXXXXXX,        XXXXXXX,        MO(_EXTRA),     KC_ENTER),
+        KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,            ADIA,
+        KC_TAB,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,           KC_H,           LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_SCLN), ODIA,
+        XXXXXXX,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,         XXXXXXX,
+        CW_TOGG,        XXXXXXX,        XXXXXXX,        XXXXXXX,        RAISE,          KC_SPACE,       XXXXXXX,        LOWER,          XXXXXXX,        XXXXXXX,        MO(_EXTRA),      KC_ENTER),
 
     [_GAMING] = LAYOUT_planck_grid(
-        KC_ESCAPE,      KC_Q,           _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,
-        _______,        KC_A,           KC_S,           KC_D,           KC_F,           _______,        _______,        KC_J,           KC_K,           KC_L,           _______,        _______,
-        KC_LEFT_SHIFT,  _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        KC_RIGHT_SHIFT,
-        _______,        KC_LEFT_GUI,    KC_LEFT_ALT,    KC_LEFT_CTRL,   LOWER,          _______,        XXXXXXX,        RAISE,          KC_LEFT_CTRL,   KC_LEFT_ALT,    _______,        _______),
+        KC_ESCAPE,      KC_Q,           _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,         _______,
+        _______,        KC_A,           KC_S,           KC_D,           KC_F,           _______,        _______,        KC_J,           KC_K,           KC_L,           _______,         _______,
+        KC_LEFT_SHIFT,  _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,         KC_RIGHT_SHIFT,
+        _______,        KC_LEFT_GUI,    KC_LEFT_ALT,    KC_LEFT_CTRL,   LOWER,          _______,        XXXXXXX,        RAISE,          KC_LEFT_CTRL,   KC_LEFT_ALT,    _______,         _______),
 
     [_LOWER] = LAYOUT_planck_grid(
-        HYPER(KC_1),    KC_1,           KC_2,           KC_3,           KC_4,           HYPER(KC_A),    HYPER(KC_D),    KC_PAGE_UP,     KC_UP,          KC_HOME,        KC_SCRL,        HYPER(KC_3),
-        HYPER(KC_2),    KC_5,           KC_6,           KC_7,           KC_8,           HYPER(KC_B),    HYPER(KC_E),    KC_LEFT,        KC_DOWN,        KC_RIGHT,       XXXXXXX,        HYPER(KC_4),
-        _______,        KC_J,           KC_K,           KC_9,           KC_0,           HYPER(KC_C),    HYPER(KC_F),    KC_PGDN,        XXXXXXX,        KC_END,         XXXXXXX,        _______,
-        _______,        _______,        _______,        _______,        _______,        KC_BSPC,        XXXXXXX,        _______,        _______,        _______,        _______,        _______),
+        HYPR(KC_1),     KC_1,           KC_2,           KC_3,           KC_4,           HYPR(KC_A),     HYPR(KC_D),     KC_PAGE_UP,     KC_UP,          KC_HOME,        KC_SCRL,         HYPR(KC_3),
+        HYPR(KC_2),     KC_5,           KC_6,           KC_7,           KC_8,           HYPR(KC_B),     HYPR(KC_E),     KC_LEFT,        KC_DOWN,        KC_RIGHT,       XXXXXXX,         HYPR(KC_4),
+        _______,        KC_J,           KC_K,           KC_9,           KC_0,           HYPR(KC_C),     HYPR(KC_F),     KC_PGDN,        XXXXXXX,        KC_END,         XXXXXXX,         _______,
+        _______,        _______,        _______,        _______,        _______,        KC_BSPC,        XXXXXXX,        _______,        _______,        _______,        _______,         _______),
 
     [_RAISE] = LAYOUT_planck_grid(
-        HYPER(KC_5),    KC_EXLM,        SE_AT,          KC_HASH,        SE_DLR,         KC_PERC,        SE_CIRC,        SE_AMPR,        SE_ASTR,        SE_DQUO,        SE_APOS,        HYPER(KC_7),
-        HYPER(KC_6),    SE_TILD,        SE_UNDS,        SE_LPRN,        SE_RPRN,        SE_PLUS,        SE_EQL,         SE_LCBR,        SE_RCBR,        SE_MINS,        SE_COLN,        HYPER(KC_8),
-        _______,        SE_GRV,         SE_PIPE,        SE_LBRC,        SE_RBRC,        SE_AA,          SE_SECT,        SE_BSLS,        SE_LESS,        SE_GRTR,        SE_QUES,        _______,
-        _______,        _______,        _______,        _______,        _______,        KC_DELETE,      XXXXXXX,        _______,        _______,        _______,        _______,        _______),
+        HYPR(KC_5),     KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_DQUO,        KC_QUOTE,        HYPR(KC_7),
+        HYPR(KC_6),     KC_TILD,        KC_UNDS,        KC_LPRN,        KC_RPRN,        KC_PLUS,        KC_EQL,         KC_LCBR,        KC_RCBR,        KC_MINS,        KC_COLN,         HYPR(KC_8),
+        _______,        KC_GRV,         KC_PIPE,        KC_LBRC,        KC_RBRC,        ARING,          XXXXXXX,        KC_BSLS,        KC_LT,          KC_GT,          KC_QUES,         _______,
+        _______,        _______,        _______,        _______,        _______,        KC_DELETE,      XXXXXXX,        _______,        _______,        _______,        _______,         _______),
 
     [_ADJUST] = LAYOUT_planck_grid(
-        _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MS_UP,       XXXXXXX,        XXXXXXX,        _______,
-        _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_WH_UP,    _______,
-        _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MS_BTN1,     KC_MS_BTN3,     KC_MS_BTN2,     KC_MS_WH_DOWN,  _______,
-        _______,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,        _______,        _______,        _______,        _______,        _______),
+        _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MS_UP,       XXXXXXX,        XXXXXXX,         _______,
+        _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_WH_UP,     _______,
+        _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MS_BTN1,     KC_MS_BTN3,     KC_MS_BTN2,     KC_MS_WH_DOWN,   _______,
+        _______,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,        _______,        _______,        _______,        _______,         _______),
 
     [_EXTRA] = LAYOUT_planck_grid(
-        _______,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          XXXXXXX,        TG(_GAMING),    XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        QK_BOOT,
-        _______,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          XXXXXXX,        LED_LEVEL,      XXXXXXX,        RGB_VAD,        RGB_VAI,        XXXXXXX,        _______,
-        _______,        KC_F9,          KC_F10,         KC_F11,         KC_F12,         XXXXXXX,        XXXXXXX,        PREV_TRACK,     PAUSE,          NEXT_TRACK,     XXXXXXX,        _______,
-        SLEEP,          XXXXXXX,        _______,        _______,        _______,        _______,        XXXXXXX,        _______,        _______,        _______,        _______,        _______),
+        _______,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          XXXXXXX,        TG(_GAMING),    XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,         QK_BOOT,
+        _______,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          XXXXXXX,        LED_LEVEL,      XXXXXXX,        RGB_VAD,        RGB_VAI,        XXXXXXX,         _______,
+        _______,        KC_F9,          KC_F10,         KC_F11,         KC_F12,         XXXXXXX,        XXXXXXX,        PREV_TRACK,     PAUSE,          NEXT_TRACK,     XXXXXXX,         _______,
+        SLEEP,          XXXXXXX,        _______,        _______,        _______,        _______,        XXXXXXX,        _______,        _______,        _______,        _______,         _______),
 };
 
 extern rgb_config_t rgb_matrix_config;
