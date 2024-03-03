cc97820501f3257444d1ad8a16497f90ad1cf370
aarnej <aarnej@iki.fi>
2024-03-03 12:16:41 +0200

    copy paste

--- keyboards/planck/ez/glow/keymaps/aarne/keymap.c
+++ keyboards/planck/ez/glow/keymaps/aarne/keymap.c
@@ -45,12 +45,16 @@ const uint32_t PROGMEM unicode_map[] = {
 #define ODIA UP(ODIA_LOWER, ODIA_UPPER)
 #define ARING UP(ARING_LOWER, ARING_UPPER)
 
+#define COPY C(KC_C)
+#define CUT C(KC_X)
+#define PASTE C(S(KC_V))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_planck_grid(
         KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,            ADIA,
         KC_TAB,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,           KC_H,           LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_SCLN), ODIA,
         XXXXXXX,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,         XXXXXXX,
-        CW_TOGG,        XXXXXXX,        XXXXXXX,        XXXXXXX,        RAISE,          KC_SPACE,       XXXXXXX,        LOWER,          XXXXXXX,        XXXXXXX,        MO(_EXTRA),      KC_ENTER),
+        CW_TOGG,        COPY,           CUT,            PASTE,          RAISE,          KC_SPACE,       XXXXXXX,        LOWER,          XXXXXXX,        XXXXXXX,        MO(_EXTRA),      KC_ENTER),
 
     [_GAMING] = LAYOUT_planck_grid(
         KC_ESCAPE,      KC_Q,           _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,         _______,
