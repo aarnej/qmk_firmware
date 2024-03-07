4fd19ee931262ac1ee57f72152200968d43a579d
aarnej <aarnej@iki.fi>
2024-03-07 14:05:27 +0200

    ...

--- keyboards/voyager/keymaps/aarne/keymap.c
+++ keyboards/voyager/keymaps/aarne/keymap.c
@@ -11,9 +11,6 @@ enum planck_layers {
     _ADJ,
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-
 #define SLEEP KC_SYSTEM_SLEEP
 #define PREV_TRACK KC_MEDIA_PREV_TRACK
 #define NEXT_TRACK KC_MEDIA_NEXT_TRACK
@@ -41,25 +38,30 @@ const uint32_t PROGMEM unicode_map[] = {
 #define ODIA UP(ODIA_LOWER, ODIA_UPPER)
 #define ARING UP(ARING_LOWER, ARING_UPPER)
 
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
+#define DEL_R LT(_RAISE, KC_DEL)
+#define BSPC_L LT(_LOWER, KC_BSPC)
+
 #define COPY C(KC_C)
 #define CUT C(KC_X)
 #define PASTE C(S(KC_V))
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_voyager(
     COPY,           CUT,            PASTE,          _______,        _______,        KC_INS,                _______,        _______,        _______,        _______,        _______,         _______,
     KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,            ADIA,
     KC_TAB,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,                  KC_H,           LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_SCLN), ODIA,
-    CW_TOGG,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                  KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,         KC_ENT,
-                                                                       KC_SPC,         RAISE,          LOWER,          KC_BSPC
+    CW_TOGG,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                  KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,         _______,
+                                                                       DEL_R,       KC_SPC,          KC_ENT,          BSPC_L
   ),
   [_GAME] = LAYOUT_voyager(
     _______,        _______,        _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
     KC_ESC,         KC_Q,           _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
     _______,        KC_A,           KC_S,           KC_D,           KC_F,           _______,               _______,        KC_J,           KC_K,           KC_L,           _______,         _______,
     KC_LSFT,        KC_LGUI,        KC_LALT,        KC_LCTL,        _______,        _______,               _______,        _______,        KC_LCTL,        KC_LALT,        _______,         KC_RSFT,
-                                                                      _______,        LOWER,          RAISE,          _______
+                                                                        LOWER,        _______,          _______,          RAISE
   ),
   [_LOWER] = LAYOUT_voyager(
     _______,        _______,        _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
