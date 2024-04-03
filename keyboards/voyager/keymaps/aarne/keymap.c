39d594fe0d85ee94138758fe908bb5c79568d1c5
aarnej <aarnej@iki.fi>
2024-04-03 20:42:29 +0300

    mousekeys

--- keyboards/voyager/keymaps/aarne/keymap.c
+++ keyboards/voyager/keymaps/aarne/keymap.c
@@ -48,13 +48,13 @@ const uint32_t PROGMEM unicode_map[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_voyager(
     _______,        _______,        COPY,           CUT,            PASTE,          KC_INS,                TG(_GAME),      _______,        _______,        KC_F2,          KC_F5,           KC_F12,
-    KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,            ADIA,
-    KC_TAB,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   LT(_ADJ, KC_G),        LT(_ADJ, KC_H), LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_SCLN), ODIA,
-    CW_TOGG,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                  KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,         _______,
+    KC_ESC,         KC_Q,           KC_W,           KC_E,           LT(_ADJ, KC_R), KC_T,                  KC_Y,           LT(_ADJ, KC_U), KC_I,           KC_O,           KC_P,            ADIA,
+    KC_TAB,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,                  KC_H,           LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_SCLN), ODIA,
+    CW_TOGG,        KC_Z,           KC_X,           KC_C,           HYPR_T(KC_V),   KC_B,                  KC_N,           HYPR_T(KC_M),   KC_COMM,        KC_DOT,         KC_SLSH,         _______,
                                                                        RAISE,         KC_SPC,         KC_ENT,      LOWER
   ),
   [_GAME] = LAYOUT_voyager(
-    _______,        _______,        _______,        _______,        _______,        _______,               _______,      _______,        _______,        _______,        _______,         _______,
+    _______,        _______,        _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
     _______,        _______,        _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
     KC_LSFT,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                  KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,         _______,
     KC_LCTL,        _______,        _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_voyager(
     _______,        _______,        _______,        _______,        _______,        _______,               _______,        _______,        _______,        _______,        _______,         _______,
     HYPR(KC_1),     KC_1,           KC_2,           KC_3,           KC_4,           HYPR(KC_A),            HYPR(KC_D),     KC_PGUP,        KC_UP,          KC_HOME,        KC_SCRL,         HYPR(KC_3),
-    KC_LALT,        KC_5,           KC_6,           KC_7,           KC_8,           HYPR(KC_B),            HYPR(KC_E),     KC_LEFT,        KC_DOWN,        KC_RIGHT,       XXXXXXX,         HYPR(KC_4),
-    _______,        KC_J,           KC_K,           KC_9,           KC_0,           HYPR(KC_C),            HYPR(KC_F),     KC_PGDN,        XXXXXXX,        KC_END,         XXXXXXX,         _______,
+    HYPR(KC_2),     KC_5,           KC_6,           KC_7,           KC_8,           HYPR(KC_B),            HYPR(KC_E),     KC_LEFT,        KC_DOWN,        KC_RIGHT,       XXXXXXX,         HYPR(KC_4),
+    KC_LALT,        KC_J,           KC_K,           KC_9,           KC_0,           HYPR(KC_C),            HYPR(KC_F),     KC_PGDN,        XXXXXXX,        KC_END,         XXXXXXX,         _______,
                                                                        _______,        _______,        _______,        _______
   ),
   [_RAISE] = LAYOUT_voyager(
@@ -76,10 +76,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_ADJ] = LAYOUT_voyager(
     XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,               XXXXXXX,        LED_LEVEL,      RGB_VAD,        RGB_VAI,        XXXXXXX,         QK_BOOT,
-    XXXXXXX,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          XXXXXXX,               XXXXXXX,        XXXXXXX,        KC_MS_U,        XXXXXXX,        XXXXXXX,         XXXXXXX,
-    XXXXXXX,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          XXXXXXX,               XXXXXXX,        KC_MS_L,        KC_MS_D,        KC_MS_R,        KC_WH_U,         XXXXXXX,
-    XXXXXXX,        KC_F9,          KC_F10,         KC_F11,         KC_F12,         XXXXXXX,               XXXXXXX,        KC_BTN1,        KC_BTN3,        KC_BTN2,        KC_WH_D,         XXXXXXX,
-                                                                      XXXXXXX,        XXXXXXX,         XXXXXXX,        XXXXXXX
+    XXXXXXX,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          XXXXXXX,               XXXXXXX,        KC_WH_U,        KC_MS_U,        KC_WH_D,        XXXXXXX,         XXXXXXX,
+    XXXXXXX,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          XXXXXXX,               XXXXXXX,        KC_MS_L,        KC_MS_D,        KC_MS_R,        XXXXXXX,         XXXXXXX,
+    XXXXXXX,        KC_F9,          KC_F10,         KC_F11,         KC_F12,         XXXXXXX,               XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,         XXXXXXX,
+                                                                      KC_ACL0,         XXXXXXX,         KC_BTN1,        KC_BTN2
   ),
 };
 
